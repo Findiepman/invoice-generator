50a1b20a9ea0c93640807c18fc91de0b1649e41d
Fin <shadowyeet23@gmail.com>
2026-03-22 20:44:17 +0100

    Added preview logic

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,11 +1,32 @@
 import type { Invoice } from "../types/types";
 
+
 interface PreviewProps {
-  invoice: Invoice;
+    invoice: Invoice;
 }
 
 export function Preview({ invoice }: PreviewProps) {
-  return (
-    <div>Hello from the preview!</div>
-  );
+    return (
+        <main>
+            <p>{invoice.Company.companyName}</p>
+            <p>{invoice.Company.companyEmail}</p>
+            <p>{invoice.Company.companyAdress}</p>
+
+            <p>{invoice.Customer.customerName}</p>
+            <p>{invoice.Customer.customerEmail}</p>
+            <p>{invoice.Customer.customerAdress}</p>
+
+            <p>{invoice.invoiceNumber}</p>
+            <p>{invoice.date}</p>
+            <p>{invoice.paymentTerms}</p>
+            <p>{invoice.vat}</p>
+            {invoice.items.map((item, index) => (
+                <div key={index}>
+                    <p>{item.name}</p>
+                    <p>${item.price * item.quantity}</p>
+                    <p>{item.quantity}</p>
+                </div>
+            ))}
+        </main>
+    );
 }
