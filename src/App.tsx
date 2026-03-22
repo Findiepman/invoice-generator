70634528691efdbcae75eb59f9ef07663153b0e0
Fin <shadowyeet23@gmail.com>
2026-03-22 20:08:34 +0100

    Set up the invoice state

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { useState } from "react"
+import type { Invoice } from "./types/types"
+
 function App() {
-  return(
+  const [invoice, setInvoice] = useState<Invoice>({
+    Customer: { customerName: '', customerAdress: '', customerEmail: '' },
+    Company: { companyName: '', companyAdress: '', companyEmail: '' },
+    items: [],
+    invoiceNumber: 1,
+    date: '',
+    paymentTerms: '',
+    vat: 0,
+  });
+
+  return (
     <div>Hello world</div>
   )
 }
