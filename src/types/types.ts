8d94454720bbfcbdf95456186513897ec70ba24f
Fin <shadowyeet23@gmail.com>
2026-03-22 20:04:45 +0100

    Set up initial types

---
+++ src/types/types.ts
@@ -0,0 +1,25 @@
+export interface Customer {
+    customerName: string,
+    customerAdress: string,
+    customerEmail: string,
+}
+export interface Company {
+    companyName: string
+    companyAdress: string
+    companyEmail: string
+}
+export interface Item {
+    price: number
+    name: string
+    quantity: number
+}
+
+export interface Invoice {
+    Customer: Customer
+    Company: Company
+    items: Item[]
+    invoiceNumber: number
+    date: string
+    paymentTerms: string
+    vat: number
+}
